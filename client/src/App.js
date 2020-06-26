fe66773ac59e796710140a21267477e0c7066893
Noor Al Darraji <49676151+nooraldarraji@users.noreply.github.com>
2020-06-25 23:16:20 -0500

    Added buttons to have more controls of the game and game rules :hammer:

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import produce from 'immer';
 
 // Grid and columns lines count
-const Rows = 40;
+const Rows = 30;
 const Columns = 50;
 
 // Main div styling here
@@ -20,6 +20,42 @@ const SimulationGrid = styled.div`
   border: solid 1px #635E5D;
   background-color: ${props => props.bg};
 `
+const ButtonsDiv = styled.div`
+  display: flex;
+  place-content: center;
+`
+const HeaderDiv = styled.div`
+  display: flex;
+  place-content: center;
+  flex-direction: row-reverse;
+  color: #ffffff;
+`
+
+const Button = styled.button`
+  box-shadow:inset 0px 1px 0px 0px #cf866c;
+  background:linear-gradient(to bottom, #d0451b 5%, #bc3315 100%);
+  background-color:#d0451b;
+  margin: 20px;
+  border-radius:3px;
+  border:1px solid #942911;
+  display:inline-block;
+  cursor:pointer;
+  color:#ffffff;
+  font-family:Arial;
+  font-size:13px;
+  padding:6px 24px;
+  text-decoration:none;
+  text-shadow:0px 1px 0px #854629;
+
+  &:hover {
+    background:linear-gradient(to bottom, #bc3315 5%, #d0451b 100%);
+    background-color:#bc3315;
+  }
+  &:active {
+    position:relative;
+    top:1px;
+  }
+`
 
 const op = [
   [0, 1],
@@ -32,12 +68,23 @@ const op = [
   [-1, 0]
 ]
 
+
+const newGrid = () => {
+  return Array.from({ length: Rows }).map(() => Array.from({ length: Columns }).fill(0))
+}
+
+const randomGame = () => {
+  // const randomNums = Math.random() > .5 ? 1 : 0
+  return Array.from({ length: Rows }).map(() => Array.from({ length: Columns }).fill(Math.random() > .5 ? 1 : 0))
+}
+
+
 function App() {
   /* 
     Here i'm creating a new state and creating two arrays filled with zeros
     to display it later with for loop inside the return of the component.
   */
-  const [gameGrid, setGameGrid] = useState(Array.from({ length: Rows }).map(() => Array.from({ length: Columns }).fill(0)));
+  const [gameGrid, setGameGrid] = useState(newGrid);
 
   /* 
     I'm using use state again here to set the initial App run state to be 
@@ -101,33 +148,46 @@ function App() {
     }
   }
   return (
-    <MainGrid
-      onClick={() => {
-        setTimeout(startOnClick, 800)
-      }}
-    >
-
-      {gameGrid.map((rows, i) =>
-        rows.map((col, j) =>
-          <SimulationGrid
-            /*
-              Set the grid square to either die or live using produce to mutate
-            */
-            bg={gameGrid[i][j] ? 'white' : undefined}
-            key={`${i}-${j}`}
-            onClick={() => {
-              const newGrid = produce(gameGrid, gridCopy => {
-                gridCopy[i][j] = gameGrid[i][j] ? 0 : 1;
-              })
+    <>
+      <HeaderDiv>
+        <h1>Conway Game of Life</h1><br />
+        <ul>
+          <h4>Game Rules:</h4>
+          <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
+          <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
+          <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
+          <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+          <li>Lives cells "wrap" around their environment and continue through to the other side.</li>
+        </ul>
+      </HeaderDiv>
+      <MainGrid>
+        {gameGrid.map((rows, i) =>
+          rows.map((col, j) =>
+            <SimulationGrid
               /*
-                Setting the state to be the old and the new state that we got from "newGrid"
-              */
-              setGameGrid(newGrid)
-            }}
-          >
-          </SimulationGrid>))}
+                Set the grid square to either die or live using produce to mutate
+                */
+              bg={gameGrid[i][j] ? 'white' : undefined}
+              key={`${i}-${j}`}
+              onClick={() => {
+                const newGrid = produce(gameGrid, gridCopy => {
+                  gridCopy[i][j] = gameGrid[i][j] ? 0 : 1;
+                })
+                /*
+                  Setting the state to be the old and the new state that we got from "newGrid"
+                */
+                setGameGrid(newGrid)
+              }}
+            >
+            </SimulationGrid>))}
 
-    </MainGrid>
+      </MainGrid>
+      <ButtonsDiv >
+        <Button onClick={() => startOnClick()}>{start ? 'STOP' : 'START'}</Button>
+        <Button onClick={() => setGameGrid(randomGame())}>RANDOM</Button>
+        <Button onClick={() => setGameGrid(newGrid())}>RESET GRID</Button>
+      </ButtonsDiv>
+    </>
   );
 }
 
